fa0c9b66d91e121916f34d654295968a8860fc58
Pedro Guemes <saldunajose@gmail.com>
2023-12-06 14:36:56 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const searchForm = document.getElementById('search-form');
+const searchBox = document.getElementById('search-box');
+const searchResult = document.getElementById('search-results');
+const showMoreBtn = document.getElementById('show-more-btn');
+const accessKey = '0EVDFdJX_Yx3WWDUh2ONEPvI732kKecjKC8K1SlWORY';
+//0EVDFdJX_Yx3WWDUh2ONEPvI732kKecjKC8K1SlWORY accessKeyAPI
+//https://api.unsplash.com/search/photos?page=1&query=office&client_id=0EVDFdJX_Yx3WWDUh2ONEPvI732kKecjKC8K1SlWORY
+
+let keyword = '';
+let page = 1;
+
+async function searchImages() {
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  if (page === 1) {
+    searchResult.innerHTML = '';
+  }
+  const results = data.results;
+  results.map((result) => {
+    const image = document.createElement('img');
+    image.src = result.urls.small;
+    const imageLink = document.createElement('a');
+    imageLink.href = result.links.html;
+    imageLink.target = '_blank';
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  });
+
+  showMoreBtn.style.display = 'block';
+
+  console.log(data);
+}
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMoreBtn.addEventListener('click', () => {
+  page++;
+  searchImages();
+});
